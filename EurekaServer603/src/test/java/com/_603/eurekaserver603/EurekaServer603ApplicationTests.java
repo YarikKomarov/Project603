db147196b20fe8adb76817d98d1db5e14829cfe2
Комаров Ярослав <music4yarik@gmail.com>
2025-03-06 12:17:03 +0300

    init commit for Eurika service
    created Eurika service

---
+++ EurekaServer603/src/test/java/com/_603/eurekaserver603/EurekaServer603ApplicationTests.java
@@ -0,0 +1,12 @@
+package com._603.eurekaserver603;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServer603ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
